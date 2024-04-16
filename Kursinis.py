844f50e67be47889aa68e14877738dde32a6a019
Pok1haya <165362788+Pok1haya@users.noreply.github.com>
2024-04-16 19:51:46 +0300

    Add files via upload

--- Kursinis.py
+++ Kursinis.py
@@ -1,16 +1,19 @@
-#komandu list'as "/help" 
-#importuot egzistuojanti sheeta arba sukurt nauja
+#komandu list'as "/help" +
+#importuot egzistuojanti sheeta + 
+#sukurt nauja sheet'a
 #sheeta editint
 #pakeist health skaiciu
 #inventorius
 #ability list
 #edit statistics
-#dice roller
+#dice roller +
 
 #cmd puslapis
 #https://medium.com/@noransaber685/simple-guide-to-creating-a-command-line-interface-cli-in-python-c2de7b8f5e05
 
 import cmd
+import random
+
 class SheetImporter:
     
     @staticmethod
@@ -25,8 +28,33 @@ class SheetImporter:
             print("File not found.")
         except Exception as e:
             print("An error occurred:", e)
+
+class DiceRoller:
+    def __init__(self):
+        self.dice_types = {
+            'd4': 4,
+            'd6': 6,
+            'd8': 8,
+            'd10': 10,
+            'd12': 12,
+            'd20': 20,
+            'd100': 10
+        }
+
+    def roll_dice(self, dice_type):
+        if dice_type.lower() in self.dice_types:
+            max_value = self.dice_types[dice_type.lower()]
+            if dice_type.lower() == 'd100':
+                result = random.randint(0, 10) * 10 
+            else:
+                result = random.randint(1, max_value)
+            print(f"Rolling {dice_type}: {result}\n")
+        else:
+            print("Invalid dice type. Choose from d4, d6, d8, d10, d12, d20, or d100.\n")
     
-class CommandInterface(cmd.Cmd, SheetImporter):
+  
+    
+class CommandInterface(cmd.Cmd):
     prompt = '> '
     intro = 'Welcome to DND helper. Type "help" for available commands.'
     
@@ -47,6 +75,15 @@ class CommandInterface(cmd.Cmd, SheetImporter):
     def do_SheetName(self, line):
         """Displays sheet name"""
         print(self.sheetName)
+    
+    def do_RollDice(self, line):
+        """Roll a dice of your choice"""
+        roller = DiceRoller()
+        while True:
+            dice_input = input("Enter the type of dice to roll (e.g., d6, d20, d100), or 'exit' to exit: ")
+            if dice_input.lower() == 'exit':
+                break
+            roller.roll_dice(dice_input)
     
     def do_Quit(self, line):
         """Exits DND helper"""
