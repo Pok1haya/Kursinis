0c1dc8f49f9e39160ae53a1090e7b8e463005cbe
Pok1haya <165362788+Pok1haya@users.noreply.github.com>
2024-04-16 19:21:05 +0300

    Add files via upload

--- Kursinis.py
+++ Kursinis.py
@@ -7,20 +7,47 @@
 #edit statistics
 #dice roller
 
-import cmd
+#cmd puslapis
+#https://medium.com/@noransaber685/simple-guide-to-creating-a-command-line-interface-cli-in-python-c2de7b8f5e05
 
-class CommandInterface(cmd.Cmd):
+import cmd
+class SheetImporter:
+    
+    @staticmethod
+    def import_sheet(importedSheetName):
+        print("Importing importedSheetName:", importedSheetName, "\n")
+        try:
+            with open(importedSheetName, 'r') as file:
+                for line in file:
+                    print(line.strip())
+                
+        except FileNotFoundError:
+            print("File not found.")
+        except Exception as e:
+            print("An error occurred:", e)
+    
+class CommandInterface(cmd.Cmd, SheetImporter):
     prompt = '> '
     intro = 'Welcome to DND helper. Type "help" for available commands.'
     
+    def __init__(self):
+        super().__init__()
+        self.sheetName = 'No imported Sheet'
+        
     def do_SheetExplain(self, line):
         """Explains sheets text file lines"""
         print("1. Name\n2. Class\n3. Stats\n4. Health\n5. inventory\n6. Abilities\n7. History")
 
     def do_ImportSheet(self, line):
-        """Imports and existing sheet"""
-        f = open('Sheet1.txt', 'r').read()
-
+        """Imports an existing sheet"""
+        importedSheetName = input("Enter file name: ")
+        SheetImporter.import_sheet(importedSheetName)
+        self.sheetName = importedSheetName
+    
+    def do_SheetName(self, line):
+        """Displays sheet name"""
+        print(self.sheetName)
+    
     def do_Quit(self, line):
         """Exits DND helper"""
         return True
